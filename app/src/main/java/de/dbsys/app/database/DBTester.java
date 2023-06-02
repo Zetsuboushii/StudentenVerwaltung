fea7da222ba794e8c7c81658cf7303f39e622ebc
Knick21 <nick.buettner04@gmail.com>
2023-06-02 14:13:21 +0200

    updated Course to work again and 🍞-testing

--- app/src/main/java/de/dbsys/app/database/DBTester.java
+++ app/src/main/java/de/dbsys/app/database/DBTester.java
@@ -17,6 +17,8 @@ public class DBTester {
         DatabaseConnector dbc = new DatabaseConnector();
         Connection conn = dbc.getConn();
 
+
+
         Student s1 = new Student(1234567, "Nick", "Büttner", "DB Systel", 6);
         s1.createStudent(conn);
         Student s2 = new Student(1357924, "Richard", "Riesmeier", "DB Systel", 4);
@@ -24,7 +26,7 @@ public class DBTester {
 
         s1.editFname(conn, "Knickemann");
 
-        Course c1 = new Course("TINF22AI1");
+        Course c1 = new Course("TINF22AI1","185C");
         c1.createCourse(conn);
 
         s1.editCourse(conn, c1);
@@ -33,7 +35,12 @@ public class DBTester {
         System.out.println(s1.getmNr() + ": " + s1.getSname() + " " + s1.getFname() + " " + "\n\tKurs: " + s1.getCourse().getcName());
         System.out.println(s2.getmNr() + ": " + s2.getSname() + " " + s2.getFname() + " " + "\n\tKurs: " + s2.getCourse().getcName());
 
-        c1.editRoom(conn, "185C");
+        System.out.println(c1.getcName() + ": " + c1.getRoom());
+        c1.editRoom(conn, "200C");
+        System.out.println(c1.getcName() + ": " + c1.getRoom());
+
+        c1.editCname(conn,"Tinfo");
+        System.out.println(c1.getcName() + ": " + c1.getRoom());
 
         s2.delete(conn);
 
