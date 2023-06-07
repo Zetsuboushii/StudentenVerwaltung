9956b7dea948553c1f6498b33e91ba71881c227a
TreSet <hanneshekl@gmai.com>
2023-06-07 09:38:52 +0200

    Added saving and loading for Student form 🥬

--- app/src/main/java/de/dbsys/app/ui/views/NewStudentViewController.java
+++ app/src/main/java/de/dbsys/app/ui/views/NewStudentViewController.java
@@ -1,10 +1,17 @@
 package de.dbsys.app.ui.views;
 
+import de.dbsys.app.database.DatabaseCrawler;
+import de.dbsys.app.database.entities.Student;
 import de.dbsys.app.ui.GenericUIController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
+import java.sql.SQLException;
+
 public class NewStudentViewController extends GenericUIController {
+    @FXML private StudentFormFieldsController studentFormFieldsController;
+
     @FXML
     private void onCancel() {
         stage.close();
@@ -12,11 +19,25 @@ public class NewStudentViewController extends GenericUIController {
 
     @FXML
     private void onSave() {
+        if(!studentFormFieldsController.isComplete()) {
+            new Alert(Alert.AlertType.ERROR, "Bitte das Formular vollständig ausfüllen.").show();
+            return;
+        }
+        Student student = studentFormFieldsController.toNewStudent();
+        // TODO: Alert failure
+        student.createStudent(Main.getDb(), Main.getDb().getConn());
         stage.close();
     }
 
     @Override
     public void onAfterShow(Stage stage) {
+        studentFormFieldsController.onAfterShow(stage);
+        try {
+            studentFormFieldsController.setStudent(new DatabaseCrawler().selectAllStudents(Main.getDb().getConn()).get(0));
+            studentFormFieldsController.populate();
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "Fehler beim Laden des Studenten.").show();
+        }
         stage.minWidthProperty().setValue(400);
     }
 }
