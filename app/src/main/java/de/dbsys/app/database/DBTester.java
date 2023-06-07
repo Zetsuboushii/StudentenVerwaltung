7332a6dc5f811d3aa4a52a2f21f72d0a4552849a
Knick21 <nick.buettner04@gmail.com>
2023-06-07 12:48:51 +0200

    quick fix in dbtester

--- app/src/main/java/de/dbsys/app/database/DBTester.java
+++ app/src/main/java/de/dbsys/app/database/DBTester.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class DBTester {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, NoCourseException {
 
 
 
